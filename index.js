1ef80361c9b9482658e2359c8b401df77272b18c
Amandine V <amavelamala@gmail.com>
2019-01-25 04:38:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart = [];
+/* var cart = [];
 
 function getCart() {
  return cart;
@@ -9,14 +9,14 @@ function setCart(c) {
   return cart;
 }
 
-/*function addToCart(item) {
+function addToCart(item) {
   var itemPrice = Math.ceil(Math.random() * 100);
   var itemObject = {itemName : item,
                     price :itemPrice
                   };
   cart.push(itemObject);
   return `${itemObject.itemName} has been added to your cart.`
-}*/
+}
 
 
 function addToCart(item) {
@@ -69,3 +69,82 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
+*/
+
+var cart = []
+
+
+ function setCart(newCart) {	function setCart(newCart) {
+  cart = newCart;	  cart = newCart;
+@@ -15,3 +15,70 @@ function total() {
+
+
+   return t	  return t
+}	}
+
+ function getCart(){
+  return cart
+}
+
+ function addToCart(item){
+var price = Math.floor(Math.random()*99)
+cart.push(new Object({[item]:price}))
+console.log(`${item} has been added to your cart.`)
+return cart
+
+ }
+
+ function viewCart (){
+if(cart.length === 0){
+  console.log(`Your shopping cart is empty.`)
+}
+else{
+      var myString = "In your cart, you have "
+
+         for(var i = 0, l = cart.length; i < l; i++){
+            for(var list in cart[i]){
+            myString+=`${list} at $${cart[i][list]}`
+                if(i!==cart.length-1){
+                  myString+=", "
+                }
+                else{
+                  myString+="."
+                }
+              }
+
+             }
+
+       console.log(myString)
+  }
+
+ }
+
+ function removeFromCart(item){
+
+   for(var i = 0, l = cart.length; i < l; i++){
+
+   for(var list in cart[i]){
+      if(item === list){
+        cart.splice(i,1)
+        return cart
+       }
+    }
+  }
+
+   console.log("That item is not in your cart.")
+  return cart
+}
+
+ function placeOrder(cardNumber) {
+
+ if (cardNumber === undefined){
+  console.log("We don't have a credit card on file for you to place your order.")
+}
+else{
+
+   console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  cart = []
+  return cart
+}
+
+ }
