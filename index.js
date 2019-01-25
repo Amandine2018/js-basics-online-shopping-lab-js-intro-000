e62eff7f607a6f502f8037b2b63d53c2339643dd
Amandine V <amavelamala@gmail.com>
2019-01-25 15:00:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,14 +64,14 @@ function total() {
 }
 
 function removeFromCart(item) {
+  var message;
   for (let i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
       cart.splice(i, 1);
       return viewCart();
-    } else {
-      return("That item is not in your cart.");
     }
   }
+  return ("That item is not in your cart.");
 }
 
 function placeOrder(cardNumber) {
