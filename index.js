96a79c20be4d01975f4ce5bb86dc3f1697f21ef3
Amandine V <amavelamala@gmail.com>
2019-01-25 03:14:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  
+
   cart.push{itemName : item, price : Math.floor((Math.random() * 100) + 1)}
                   }
   return `${itemObject.itemName} has been added to your cart.`
