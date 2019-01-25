9bf36e6de4230ba5dbfd683795ef33358f1eb989
Amandine V <amavelamala@gmail.com>
2019-01-25 14:29:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (! cardNumber) {
-    console.log("Sorry, we don't have a credit card on file for you.")
+    return("Sorry, we don't have a credit card on file for you.")
   } else {
     var tot = total();
     setCart(0);
